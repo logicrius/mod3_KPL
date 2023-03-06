0e823289902eab2242adde91624219aefbe72253
logicrius <asharsyahbudi@student.telkomuniversity.ac.id>
2023-03-06 07:06:48 +0700

    mod3

---
+++ JURNAL_MOD3_13022210070_AsharSyahbudiSaleh/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JURNAL_MOD3_13022210070_AsharSyahbudiSaleh
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
